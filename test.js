e7e698ebf012afadd151171706b5c94a2748a936
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2018-01-30 20:18:30 -0600

    added json parse and stringify

--- test.js
+++ test.js
@@ -1,13 +1,5 @@
 
-require("./lib/components/just");
-require("./lib/components/selectShallow");
-require("./lib/components/merge");
-require("./lib/components/label");
-require("./lib/components/to-array");
-require("./lib/components/accumulative-merge-object");
-require("./lib/components/take");
-require("./lib/components/inlet");
-require("./lib/components/from-pipe-registry")
+require("./index")
 
 var PiperTron = require("pipertron").PiperTron;
 
@@ -15,13 +7,13 @@ var libs = {Rx: require("@reactivex/rxjs"), PipeRegistry:{}, inlets:{}};
 var parser = PiperTron.buildFromRegistry(libs);
 libs.pipertronInstance = parser;
 
-libs.PipeRegistry.test = 'INLET "<INLET_ID>" | LABEL "impressed"'
+libs.PipeRegistry.test = '| LABEL "impressed"'
 
 
 parser.execute('JUST {"hello": "world"} | SELECT_SHALLOW "hello" | LABEL "people" | MERGE ( JUST 5 | MERGE ( JUST 5 ) | LABEL "stuff" | TAKE 2 ) | ACCUMULATIVE_MERGE_OBJECTS ').subscribe((v) => {
   console.log(v)
 });
 
-parser.execute('JUST {"hello": "world"} | MERGE ( JUST 7 | FROM_PIPE_REGISTRY "test" ) | ACCUMULATIVE_MERGE_OBJECTS').subscribe((v) => {
+parser.execute('JUST {"hello": "world"} | MERGE ( JUST 7 | USE_PIPE_REGISTRY "test" ) | ACCUMULATIVE_MERGE_OBJECTS | JSON_STRINGIFY | JSON_PARSE').subscribe((v) => {
   console.log(v)
 });
